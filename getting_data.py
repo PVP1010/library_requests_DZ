b75d82356c33c92e5559f0d1d939d17d4486fc02
PVP1010 <Valera10102791@mail.ru>
2024-10-14 15:40:51 +0700

    Update getting_data.py

--- getting_data.py
+++ getting_data.py
@@ -7,6 +7,6 @@ params = {
 
 response = requests.get( "https://api.github.com/search/repositories", params=params)
 
-print(response.status_code)
 response_json = response.json()
 pprint.pprint(response_json)
+print(response.status_code)
